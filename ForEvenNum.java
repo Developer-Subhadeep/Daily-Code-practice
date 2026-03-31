69ffe15511ccceab1542a8b414736db5f63513b5
Subhadeep Samal <Hellosubhadeep32@gmail.com>
2026-04-01 01:44:09 +0530

    Extra: Second program practice

---
+++ ForEvenNum.java
@@ -0,0 +1,21 @@
+class ForEvenNum 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+		/*
+		Write a program using a for
+		loop to print even numbers from 1 to 20
+		*/
+
+		for(int i=1;i<=10; i++){
+			if(i%2!=0)
+			{
+				//break
+				continue;
+			}
+			System.out.println(i); 
+		}
+
+	}
+}
